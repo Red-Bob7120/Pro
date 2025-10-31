a375580e0f06c192ffe65fd9dbebc38b4876ba53
Red-Bob7120 <pjh93100@gmail.com>
2025-10-31 09:29:47 +0900

    react_mid_problems

---
+++ React/react_mid_problems/problem1/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section>AmountInput</section>
+      <section>CurrencySelector</section>
+      <section>ConversionResult</section>
+    </div>
+  );
+}
+
+export default App;
