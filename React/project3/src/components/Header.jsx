2867ef7892617012f85c25d188c8ad40f6a1bc29
Red-Bob7120 <pjh93100@gmail.com>
2025-11-07 11:04:39 +0900

    Header.jsx add .memo

--- React/project3/src/components/Header.jsx
+++ React/project3/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import "./Header.css";
+import React from"react";
 const Header = () => {
+    // console.log("Header")
     return <div className="Header">
         <h3>오늘은 </h3>
         <h1>{new Date().toDateString()}</h1>
     </div>;
 };
-export default Header;
+export default React.memo(Header);
